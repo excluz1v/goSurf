cb4f9f1832e70fb62e675a71be9e9ea997153da9
excluz1v <bmd4work@gmail.com>
2020-10-01 00:39:24 +0600

    2 section

--- app/js/main.js
+++ app/js/main.js
@@ -17,7 +17,27 @@ $(function () {
         slidesToScroll: 1,
         slidesToShow: 4,
         prevArrow: '<button type="button" aria-label="previous slide" class="slick-prev shadow top40"><img src="../img/arrow-left.svg"></img></button>',
-        nextArrow: '<button type="button" class="slick-next shadow top40" aria-label="next slide"><img src="../img/arrow-right.svg"></img></button>'
+        nextArrow: '<button type="button" class="slick-next shadow top40" aria-label="next slide"><img src="../img/arrow-right.svg"></img></button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
 
     })
     $('.travel__slider').slick({
@@ -81,6 +101,50 @@ $(function () {
     $('.slider-card__state').on('click', function () {
         $(this).toggleClass('active')
     })
+    $('.menuToggle').on('click', function () {
+        $('.menu_burger').slideToggle(300, function () {
+            if ($(this).css('display') === "none") {
+                $(this).removeAttr('style');
 
+            }
+        });
+
+    });
 
 });
+
+
+const someArray = [
+    {
+        id: 1,
+        value: 15,
+    },
+    {
+        id: 2,
+        value: 3,
+    },
+    {
+        id: 1,
+        value: 4,
+    },
+];
+
+// // Отсортируй массив по возрастанию и по убыванию value.
+// someArray.sort(function (a, b) {
+//     return a.value - b.value
+// });
+// console.log(someArray)
+
+// someArray.sort()
+// // Верни массив, добавив каждому элементу новое поле.
+// someArray.map(el => {
+//     el.new = 5
+// });
+
+// // Верни только те элементы, которые имеют value больше первого в массиве.
+// someArray.filter(el => {
+//     if (el.value > someArray[0].value) {
+//         return el
+//     } else false
+// });
+
